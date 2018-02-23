668b1157cf3f83fd3a9a9696ac5e56dc6f68706c
batmunkey <sambpowell@gmail.com>
2018-02-22 17:15:20 -0700

    Update 20130227120324_add_patch_id_to_changeset.rb

--- db/migrate/20130227120324_add_patch_id_to_changeset.rb
+++ db/migrate/20130227120324_add_patch_id_to_changeset.rb
@@ -1,4 +1,4 @@
-class AddPatchIdToChangeset < ActiveRecord::Migration
+class AddPatchIdToChangeset < ActiveRecord::Migration[5.1]
   def change
     add_column :changesets, :patch_id, :string unless column_exists? :changesets, :patch_id
   end
