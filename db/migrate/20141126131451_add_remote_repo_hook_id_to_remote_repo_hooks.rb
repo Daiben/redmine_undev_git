4b577aabff6ec0029e5530ef9e102a8884bf7588
batmunkey <sambpowell@gmail.com>
2018-02-22 17:21:03 -0700

    Update 20141126131451_add_remote_repo_hook_id_to_remote_repo_hooks.rb

--- db/migrate/20141126131451_add_remote_repo_hook_id_to_remote_repo_hooks.rb
+++ db/migrate/20141126131451_add_remote_repo_hook_id_to_remote_repo_hooks.rb
@@ -1,4 +1,4 @@
-class AddRemoteRepoHookIdToRemoteRepoHooks < ActiveRecord::Migration
+class AddRemoteRepoHookIdToRemoteRepoHooks < ActiveRecord::Migration[5.1]
   def change
     add_column :remote_repo_hooks, :remote_repo_ref_id, :integer
   end
