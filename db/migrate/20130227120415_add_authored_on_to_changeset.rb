b6085308e8d89f75854e16aee217bf7d36763fc5
batmunkey <sambpowell@gmail.com>
2018-02-22 17:15:36 -0700

    Update 20130227120415_add_authored_on_to_changeset.rb

--- db/migrate/20130227120415_add_authored_on_to_changeset.rb
+++ db/migrate/20130227120415_add_authored_on_to_changeset.rb
@@ -1,4 +1,4 @@
-class AddAuthoredOnToChangeset < ActiveRecord::Migration
+class AddAuthoredOnToChangeset < ActiveRecord::Migration[5.1]
   def change
     add_column :changesets, :authored_on, :datetime unless column_exists? :changesets, :authored_on
   end
