f7f76b908487eaa8c9c3da1b8f16f482e08b5142
batmunkey <sambpowell@gmail.com>
2018-02-22 17:01:13 -0700

    Update 20130227120125_add_branches_to_changeset.rb
    Patches "Directly inheriting from ActiveRecord::Migration is not supported. Please specify the Rails release the migration was written for:" error

--- db/migrate/20130227120125_add_branches_to_changeset.rb
+++ db/migrate/20130227120125_add_branches_to_changeset.rb
@@ -1,4 +1,4 @@
-class AddBranchesToChangeset < ActiveRecord::Migration
+class AddBranchesToChangeset < ActiveRecord::Migration[5.1]
   def change
     add_column :changesets, :branches, :text unless column_exists? :changesets, :branches
   end
