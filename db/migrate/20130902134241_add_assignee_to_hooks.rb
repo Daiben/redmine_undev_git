9b683811c796b0ecedfb4b322a828f1dd74daa01
batmunkey <sambpowell@gmail.com>
2018-02-22 17:16:28 -0700

    Update 20130902134241_add_assignee_to_hooks.rb

--- db/migrate/20130902134241_add_assignee_to_hooks.rb
+++ db/migrate/20130902134241_add_assignee_to_hooks.rb
@@ -1,4 +1,4 @@
-class AddAssigneeToHooks < ActiveRecord::Migration
+class AddAssigneeToHooks < ActiveRecord::Migration[5.1]
   def change
     add_column :hooks, :new_assigned_to_id, :integer
   end
