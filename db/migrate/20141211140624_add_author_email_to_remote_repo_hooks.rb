2100cee4ef9713d6f41447ffeb2aced451148eb7
batmunkey <sambpowell@gmail.com>
2018-02-22 17:22:03 -0700

    Update 20141211140624_add_author_email_to_remote_repo_hooks.rb

--- db/migrate/20141211140624_add_author_email_to_remote_repo_hooks.rb
+++ db/migrate/20141211140624_add_author_email_to_remote_repo_hooks.rb
@@ -1,4 +1,4 @@
-class AddAuthorEmailToRemoteRepoHooks < ActiveRecord::Migration
+class AddAuthorEmailToRemoteRepoHooks < ActiveRecord::Migration[5.1]
   def change
     remove_column :remote_repo_hooks, :author_string
     add_column :remote_repo_hooks, :author_email, :string
