1ad6bdd1d55d9a0a2c40ce51ffbb00595fdec849
batmunkey <sambpowell@gmail.com>
2018-02-22 17:21:16 -0700

    Update 20141202101016_add_remote_repo_revision_id_to_time_entries.rb

--- db/migrate/20141202101016_add_remote_repo_revision_id_to_time_entries.rb
+++ db/migrate/20141202101016_add_remote_repo_revision_id_to_time_entries.rb
@@ -1,4 +1,4 @@
-class AddRemoteRepoRevisionIdToTimeEntries < ActiveRecord::Migration
+class AddRemoteRepoRevisionIdToTimeEntries < ActiveRecord::Migration[5.1]
   def change
     add_column :time_entries, :remote_repo_revision_id, :integer
   end
